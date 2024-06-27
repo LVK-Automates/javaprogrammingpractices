b0461981e7e8579ad9a78150c5aa11e26fe7f28a
LVK-Automates <lv.kirthivasan@hotmail.com>
2024-06-28 00:45:06 +0530

    Pushing the takeWhileExample and filterExample files

--- src/test/java/loops/takeWhileExample.java
+++ src/test/java/loops/takeWhileExample.java
@@ -15,24 +15,24 @@ public class takeWhileExample {
         listObj.add(6);
         listObj.add(7);
         /**
-         * In the below code, iteration will be stopped at the first element itself as the condition is becoming false so it prints nothing
+         * In the below code, matching operation will be stopped at the first element itself as the condition is becoming false so it prints nothing
          */
         listObj.stream().takeWhile( val -> val == 3).forEach(System.out::print);
 
         /**
-         * In the below code, iteration will continue till the condition is satisfying
+         * In the below code, matching operation will continue till the condition is satisfying
          * * output: 446
          */
        // listObj.stream().takeWhile( val -> val >= 3).forEach(System.out::print);
 
         /**
-         * In the below code, iteration will continue till the condition is satisfying for the first two elements. for the third element it becomes false and hence the iteration will be stopped
+         * In the below code, operation will continue till the condition is satisfying for the first two elements. for the third element it becomes false and hence the iteration will be stopped
          * output: 44
          */
         //listObj.stream().takeWhile( val -> val == 4).forEach(System.out::print);
 
         /**
-         * In the below code, iteration will continue till the condition is satisfying for the first two elements. for the seventh element it becomes false and hence the iteration will be stopped
+         * In the below code, operation will continue till the condition is satisfying for the first two elements. for the seventh element it becomes false and hence the iteration will be stopped
          * output: 446256
          */
 
